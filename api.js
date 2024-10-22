48be769d9799cfdccdf5c84f3a88a8ce21df7bca
RbL2024 <rbms.labanos2024@gmail.com>
2024-10-22 23:47:59 +0800

    update

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express()
-const PORT = process.env.port;
+const PORT = process.env.LISTEN_PORT;
 
 const admin_accounts = require('./models/admin_accounts.model');
 
--- dbconn.js
+++ dbconn.js
@@ -1,7 +1,7 @@
 const mongoose  = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.mongo_uri);
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open',  () => {
